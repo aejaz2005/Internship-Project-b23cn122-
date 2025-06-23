7a71d33794e0fc75c5e5312b9659557d84070700
aejaz2005 <mr.aejaz7@gmail.com>
2025-06-23 22:37:33 +0530

    Add files via upload
    The app.js file

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+import Main_Project from './Internship_project/Main_Project';
+
+
+
+function App(){
+  return(
+    <div>
+      <Main_Project/>
+    </div>
+  )
+}
+
+
+
+export default App;
